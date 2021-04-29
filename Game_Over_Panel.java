827a28e62dd208430c5688d39d42a8914fc003fd
YyMooMoo <83413346+YyMooMoo@users.noreply.github.com>
2021-04-29 11:14:04 -0400

    Update Game_Over_Panel.java

--- Game_Over_Panel.java
+++ Game_Over_Panel.java
@@ -1,23 +1,29 @@
    //Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
-   //Last Updated: 4/27/2021
+   //Date: 4/27/2021
    //Period: 2
-
+   import javax.swing.*;
+   import java.awt.*;
+   import java.awt.event.*;
 public class Game_Over_Panel extends JPanel
 {
-   JPanel subpanel = new JPanel();
-       subpanel.setLayout(new FlowLayout);
-       public Game_Over_Panel()
-       {
-       
-       }
-   public static void getResult
-   {
-   }
+      public Game_Over_Panel()
+      {
+            setLayout(new FlowLayout());
+            
+            JButton button1 = new JButton("Try Again");
+            button1.addActionListener(new Listener1());
+            add(button1);
+            
+            JButton button2 = new JButton("Exit");
+            button2.addActionListener(new Listener2());
+            add(button2);
+      }
    private class Listener1 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
       
       }
+    }
     
     private class Listener2 implements ActionListener{
       public void actionPerformed(ActionEvent e)
@@ -25,3 +31,7 @@ public class Game_Over_Panel extends JPanel
       
       }
      }
+    public static void getResult()
+    {
+    }
+   }
