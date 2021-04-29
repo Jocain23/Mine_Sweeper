fe19d6cffaed20799530e1ab965dd5bb6594b094
YyMooMoo <83413346+YyMooMoo@users.noreply.github.com>
2021-04-29 11:28:01 -0400

    Update Playboard_Panel.java

--- Playboard_Panel.java
+++ Playboard_Panel.java
@@ -9,14 +9,16 @@ public class Playboard_Panel extends JPanel
 {
    public Playboard_Panel()
    {
-       
+       /// Sets up the panel
    }
    public static void Initial_Generate()
    {
+      /// Generates the initial map with each square being empty
    }
    
    public static void Final_Generate()
    {
+      /// This method is called when the first square is revealed. This places all the mines. 
    }
 
    public static void Mine_Check(int x, int y)
@@ -24,18 +26,20 @@ public class Playboard_Panel extends JPanel
    }
    public static void Spread(int x, int y)
    {
+      /// All of the surrounding squares display themselves. 
+      /// It also checks for victory and tells the game over panel the result. 
    }
    private class Listener1 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
-      
+         /// Is called when the square is flagged.
       }
    }
 
    private class Listener2 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
-      
+         /// Is called when the square is clicked on.
       }
    }
 
