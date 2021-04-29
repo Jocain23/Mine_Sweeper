3843445a8b0c1fcb87e8988fc991d8acb879cd10
Jocain23 <81326119+Jocain23@users.noreply.github.com>
2021-04-29 11:11:32 -0400

    Update Minesweeper_Driver.java

--- Minesweeper_Driver.java
+++ Minesweeper_Driver.java
@@ -1,11 +1,51 @@
-   //Name: Hardeep Mann, Jonluke O'Cain, and Vayun Malik
-   //Last Updated: 4/27/2021
+   import javax.swing.*;
+   import java.awt.*;
+   import java.awt.event.*;
+   //Name: Hardeep Mann, Jonluke O'Cain, Vayun Malik
+   //Date: 4/27/2021
    //Period: 2
 
-public class Minesweeper_Driver
+public class Start_Up_Panel extends JPanel
 {
-   public static void main(String[] args)
+       public Start_Up_Panel()
+       {
+       
+       }
+       
+   public static void set_side_length()
    {
-   
    }
+   
+   public static void set_num_bombs()
+   {
+   }
+   public int get_side_length()
+   {
+   return 99;// to compile
+   }
+      public int get_num_bombs()
+   {
+   return 99;// to compile
+   }
+    private class Listener1 implements ActionListener{
+      public void actionPerformed(ActionEvent e)
+      {
+      
+      }
+    }
+
+     private class Listener2 implements ActionListener{
+      public void actionPerformed(ActionEvent e)
+      {
+      
+      }
+    }
+     private class Listener3 implements ActionListener{
+      public void actionPerformed(ActionEvent e)
+      {
+      }
+    }
+
+
+
 }
