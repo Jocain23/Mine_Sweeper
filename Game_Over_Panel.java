1dc4ab91d4d0bb599460a0433d3777aac1d13019
YyMooMoo <83413346+YyMooMoo@users.noreply.github.com>
2021-04-29 11:19:34 -0400

    Update Game_Over_Panel.java

--- Game_Over_Panel.java
+++ Game_Over_Panel.java
@@ -8,6 +8,7 @@ public class Game_Over_Panel extends JPanel
 {
       public Game_Over_Panel()
       {
+            /// sets up the panel
             setLayout(new FlowLayout());
             
             JButton button1 = new JButton("Try Again");
@@ -21,17 +22,18 @@ public class Game_Over_Panel extends JPanel
    private class Listener1 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
-      
+         /// the user can click this button to try again
       }
     }
     
     private class Listener2 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
-      
+         /// the user can click this button to exit the program
       }
      }
     public static void getResult()
     {
+      /// This method gets the result of whether its a win or a loss from the play board panel. Then it will use that result to display the correct text for the user.
     }
    }
