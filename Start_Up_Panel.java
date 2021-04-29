010c75464d9a90038117cdde01dda4af9612b550
Jocain23 <81326119+Jocain23@users.noreply.github.com>
2021-04-29 11:40:50 -0400

    Update Start_Up_Panel.java

--- Start_Up_Panel.java
+++ Start_Up_Panel.java
@@ -1,7 +1,17 @@
+/*********************************************
+The StartThis is the panel that gets the user input. 
+It will ask the user for a side-length and the number of
+mines. It will give three preset recommendations of 
+easy medium and hard that the player can choose from. 
+When these two are entered they will be saved as integer 
+variables side_length and num_bombs respectively. 
+Once the user has entered all of the information a 
+generate button will be displayed which the user can 
+click to generate the playboard and start the game.
 
-   //Name: Hardeep Mann, Jonluke O'Cain
-   //Date: 4/29/2021
-   //Period: 2
+@author Hardeep Mann, Jonluke O'Cain and Vayun Malik
+@version 0.1
+**********************************************/ 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -12,38 +22,60 @@ public class Start_Up_Panel extends JPanel
        {
        
        }
-       
+   
    public static void set_side_length()
    {
    }
-   
+   /*******************************
+   Sets the number of squares on each side
+   @param side_length
+   *******************************/
+
    public static void set_num_bombs()
    {
    }
-   public int get_side_length()
+   /*******************************
+   Sets the number of squares which will be bombs 
+   @param num_bombs
+   *******************************/
+   public int side_length()
    {
    return 99;// to compile
    }
-      public int get_num_bombs()
+      public int num_bombs()
    {
+     
    return 99;// to compile
    }
+   
     private class Listener1 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
       
       }
+     
     }
+    /****************************************
+    Serves as the user imput for set_num_bombs() 
+    *****************************************/
 
      private class Listener2 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
       
       }
+
     }
+     /****************************************
+    Serves as the user imput for set_side_length() 
+    *****************************************/
      private class Listener3 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
       }
+      /**************************************
+      Once the user has entered all of the information pressing this
+      button will generate the playboard and start the game.
+      **************************************/
     }
     }
