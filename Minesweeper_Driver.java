5112ddbd57058849dc8178eb922e94894807f62e
Hardeep Mann <33522407+HardeepMann@users.noreply.github.com>
2021-04-29 11:40:44 -0400

    Update Minesweeper_Driver.java

--- Minesweeper_Driver.java
+++ Minesweeper_Driver.java
@@ -1,15 +1,15 @@
-   	//Name: Hardeep Mann and Jonluke O'Cain
-   //Date: 4/27/2021
+   //Name: Hardeep Mann, Jonluke O'Cain, and Vayun Malik
+   //Date: 4/29/2021
    //Period: 2
    import javax.swing.JFrame;
 public class Minesweeper_Driver
 {
    //This is the class that will set everything up. 
-   //It will create the start up panel with a predetermined size.
-   //When the start up panel gets the size and number of mines from the user it will send that information to the playboard panel. 
-   //Then the driver will create the playboard panel, and then create the game over panel when the game ends.
    public static void main(String[] args)
    {
+   //This method will create the start up panel with a predetermined size.
+   //When the start up panel gets the size and number of mines from the user it will send that information to the playboard panel. 
+   //Then the driver will create the playboard panel, and then create the game over panel when the game ends.
          JFrame frame = new JFrame("Mine Sweeper");
          frame.setSize(500, 150);
          frame.setLocation(150, 100);
