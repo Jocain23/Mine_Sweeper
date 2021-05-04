b728e44eb2d77fd9d19f61c8ea28ef9a57f92065
YyMooMoo <83413346+YyMooMoo@users.noreply.github.com>
2021-05-04 11:00:50 -0400

    Update Playboard_Panel.java

--- Playboard_Panel.java
+++ Playboard_Panel.java
@@ -1,47 +1,47 @@
    //Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
-   //Last Updated: 4/29/2021
+   //Last Updated: 5/4/2021
    //Period: 2
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
-public class Playboard_Panel extends JPanel
-{
-   public Playboard_Panel()
+   import javax.swing.*;
+   import java.awt.*;
+   import java.awt.event.*;
+   public class Playboard_Panel extends JPanel
    {
-       /// Sets up the panel
-   }
-   public static void Initial_Generate()
+      public Playboard_Panel()
+      {
+         setLayout(new BorderLayout());
+      
+         JPanel panel = new JPanel();
+         panel.setLayout(new FlowLayout());
+         add(panel, BorderLayout.SOUTH);
+         
+         JButton button1 = new JButton("Generate");
+         button1.addActionListener(new Listener1());
+         panel.add(button1);
+         
+      }
+      public static void Initial_Generate()
    {
       /// Generates the initial map with each square being empty
    }
-   
-   public static void Final_Generate()
+      public static void Final_Generate()
    {
       /// This method is called when the first square is revealed. This places all the mines. 
    }
-
-   public static void Mine_Check(int x, int y)
+      public static void Mine_Check(int x, int y)
    {
       /// Starts by setting a count to 0. Then it assigns each square a number based on how many mines are in its vicinity. It then returns the count.
    }
-   public static void Spread(int x, int y)
+      public static void Spread(int x, int y)
    {
       /// All of the surrounding squares display themselves. 
       /// It also checks for victory and tells the game over panel the result. 
    }
-   private class Listener1 implements ActionListener{
-      public void actionPerformed(ActionEvent e)
+        private class Listener1 implements ActionListener
       {
-         /// Is called when the square is flagged.
+         public void actionPerformed(ActionEvent e)
+         {
+            // generates a map of empty squares
+         }
       }
-   }
-
-   private class Listener2 implements ActionListener{
-      public void actionPerformed(ActionEvent e)
-      {
-         /// Is called when the square is clicked on.
-      }
-   }
-
+       
 }
