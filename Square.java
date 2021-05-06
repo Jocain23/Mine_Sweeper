5e33abbfc558ca8a27747e9479235ce3e1b68542
Jocain23 <81326119+Jocain23@users.noreply.github.com>
2021-05-06 10:59:49 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,6 +1,8 @@
-    //Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
-   //Last Updated: 5/4/2021
-   //Period: 2
+    /*************************************************
+ Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
+ Last Updated: 5/6/2021
+Period: 2
+**************************************************/
  import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,29 +10,39 @@ import java.awt.event.*;
 
 public abstract class Square
 {
+ 
 
   
    //This class exhibits polymorphic behavior and defines the square objects. 
    //The square objects have three different states which are clicked_on, flagged, and untouched. 
    //The first state is untouched and all squares start out this way. 
 
-   boolean is_Mine;
+   public static boolean is_Mine;
+   public boolean clicked = false; 
+   // The boolean varible clicked starts false and set to true when clicked, Used for checking win conditions
    //The boolean variable is_Mine is true when the square has a mine, and false otherwise.
    public JLabel label1 = new JLabel(" ");
    public abstract void display();
    
-   public static void clicked_on()
+   public void clicked_on()
    {
       //Whenever a square is interacted with the state changes to clicked_on. 
       //In this state the square opens a menu that allows the user to either flag or reveal the square. 
       //When the square is flagged it transitions into the flagged state.
+     if (is_Mine == true)
+     {
+     Game_Over_Panel Jeff = new Game_Over_Panel();
+     }
+     
+     clicked = true;
+     this.display();
       
 
    }
 
    
-   public static void flagged()
+   public void flagged()
    {
-      //This is the state when the square is flagged.
+      label1.setText("F");
    }
 }
