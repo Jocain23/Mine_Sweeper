75c571356a70e6d6a00a7f33361e30104c9e9c74
YyMooMoo <83413346+YyMooMoo@users.noreply.github.com>
2021-05-06 10:55:14 -0400

    Update Game_Over_Panel.java

--- Game_Over_Panel.java
+++ Game_Over_Panel.java
@@ -1,16 +1,18 @@
   //Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
-   //Last Updated: 5/5/2021
+   //Last Updated: 5/6/2021
    //Period: 2
    import javax.swing.*;
    import java.awt.*;
    import java.awt.event.*;
+   import javax.swing.JFrame;
 public class Game_Over_Panel extends JPanel
 {
    //This panel is shown to the user when the game is ended.
       private JLabel label1;
+      private String result;
       public Game_Over_Panel()
       {
-            /// sets up the panel
+            // sets up the panel
             setLayout(new FlowLayout());
             
             JButton button1 = new JButton("Try Again");
@@ -21,27 +23,42 @@ public class Game_Over_Panel extends JPanel
             button2.addActionListener(new Listener2());
             add(button2);
             
-            label1 = new JLabel("The End");
-            label1.setFont(new Font("Serif", Font.BOLD, 100));
+            label1 = new JLabel(" ");
+            label1.setFont(new Font("Times New Roman", Font.BOLD, 100));
             label1.setForeground(Color.blue);
+            getResult();
             add(label1);
       }
    private class Listener1 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
-         /// the user can click this button to try again
+         // the user can click this button to try again
+         JFrame frame = new JFrame("Mine Sweeper");
+         frame.setSize(500, 200);
+         frame.setLocation(150, 100);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setContentPane(new Start_Up_Panel());
+         frame.setVisible(true);
       }
     }
     
     private class Listener2 implements ActionListener{
       public void actionPerformed(ActionEvent e)
       {
-         /// the user can click this button to exit the program
+         // the user can click this button to exit the program
          System.exit(0);
       }
      }
-    public static void getResult()
+      public void getResult()
     {
-      /// This method gets the result of whether its a win or a loss from the play board panel. Then it will use that result to display the correct text for the user.
+      result = "lose";
+      // This method gets the result of whether its a win or a loss from the play board panel. 
+      // Then it will use that result to display the correct text for the user.
+      if (result == "win")
+      {
+         label1.setText("Congratulations You Won!!");
+      }
+      else
+         label1.setText("Sorry, You Lost.");
     }
    }
