5e39d8321905cee66e7ad8568764596b04aba525
Hardeep Mann <33522407+HardeepMann@users.noreply.github.com>
2021-05-10 13:12:09 -0400

    Update Playboard_Panel.java

--- Playboard_Panel.java
+++ Playboard_Panel.java
@@ -1,83 +1,255 @@
-    //Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
-   //Last Updated: 5/8/2021
-   //Period: 2
-   import java.util.*;
-   import javax.swing.*;
-   import java.awt.*;
-   import java.awt.event.*;
-   import javax.swing.JFrame;
-   public class Playboard_Panel extends JPanel
-   {
-      public int length;
-      public int num_squares;
-      public int mines;
-      public Playboard_Panel(int side_length, int num_mines)
-      {
-         setLayout(new BorderLayout());
+
+//Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
+//Last Updated: 4/29/2021
+//Period: 2
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Playboard_Panel extends JPanel {
+   private int cellsInSide = 0;
+   private int mines = 0;
+   private Square[][] squares;
+
+   private int start_X = -1;
+   private int start_Y = -1;
+   private int numFlagsUsed = 0;
+
+   private boolean displayIcons = true;
+   private Minesweeper_Driver driver;
+   private Set<String> checked = new HashSet<String>();
+
+   public Playboard_Panel(int cellsInSide, int mines, boolean displayIcons, Minesweeper_Driver driver) {
+      this.driver = driver;
+   
+      this.cellsInSide = cellsInSide;
+      this.mines = mines;
+   
+      this.displayIcons = displayIcons;
+   
+      this.setLayout(new BorderLayout());
+      Initial_Generate();
+   }
+
+   public void Initial_Generate() {
+   
+   	/// Generates the initial map with each square
+   
+      squares = new Square[cellsInSide][cellsInSide];
+   
+      Container grid = new Container();
+      grid.setLayout(new GridLayout(cellsInSide, cellsInSide));
+   
+      for (int x = 0; x < cellsInSide; x++) {
+         for (int y = 0; y < cellsInSide; y++) {
+            squares[x][y] = new Square(x, y);
+            squares[x][y].setFont(new Font("Arial", Font.PLAIN, 12));
+            squares[x][y].addActionListener(actionListener);
+            squares[x][y].addMouseListener(mouseListener);
+            squares[x][y].displayOrig();
+            grid.add(squares[x][y]);
+         }
+      }
+   
+      this.add(grid, BorderLayout.CENTER);
+   }
+
+   public void Final_Generate() {
+      Set<String> safeSquareAndMinesLocation = new HashSet<String>();
+      safeSquareAndMinesLocation.add(start_X + " " + start_Y);
+   
+      long seed = System.currentTimeMillis();
+      Random randomX = new Random(seed);
+      Random randomY = new Random(seed * 2);
+   
+      int totalMines = 0;
+      int numTries = 0;
+   
+      while (totalMines < mines && numTries < 100000) {
       
-         JPanel panel = new JPanel();
-         panel.setLayout(new FlowLayout());
-         add(panel, BorderLayout.SOUTH);
+         numTries++;
+      
+         int x = randomX.nextInt(cellsInSide);
+         int y = randomY.nextInt(cellsInSide);
+      
+         if (safeSquareAndMinesLocation.add(x + " " + y)) {
+            totalMines++;
+            squares[x][y].setIsMine(true);
+         } else {
+         	// System.out.println("DuplicateMine NOT added "+ mines + " " + totalMines + " "
+         	// + x + " " + y);
+         }
+      }
+   
+      safeSquareAndMinesLocation.remove(start_X + " " + start_Y);
+   
+      System.out.println("First selected square at: " + start_X + " " + start_Y);
+      System.out.println("Mines are at: " + safeSquareAndMinesLocation.toString());
+   }
+
+   private final ActionListener actionListener = 
+      actionEvent -> {
+         Object source = actionEvent.getSource();
+      
+         if (start_X == -1) {
+            start_X = ((Square) source).getXLoc();
+            start_Y = ((Square) source).getYLoc();
          
-         length = side_length;
-         num_squares = length * length;
-         mines = num_mines;
+            Final_Generate();
+            Square.updateMinesTochingCount(squares);
          
-         JPanel panel2 = new JPanel();
-         panel2.setLayout(new GridLayout(length, length));
-         add(panel2, BorderLayout.NORTH); 
+         /** For debugging purposes only **/
+         /*
+         for (int x = 0; x < cellsInSide; x++) {
+         	for (int y = 0; y < cellsInSide; y++) {
+         		squares[x][y].displayForDebug();
+         	}
+         }
+         */
          
+            if (((Square) source).getMinesTouching() == 0) {
+               lookDisplaySafeSquares(((Square) source));
+            } else {
+               ((Square) source).display(displayIcons);
+               ((Square) source).setEnabled(false);
+            }
+            System.out.println("FirstTime Square selected : ActionListener Square is: " + ((Square) source).getXLoc()
+               + " " + (((Square) source).getYLoc()));
+         } else {
          
-         Square matrix[][] = new Square[length][length];
-         for(int j = 0; j < length; j++)
-         {
-            for(int t = 0; t < length; t++) 
-            {
-               matrix[t][j] = new Square(false);
-               JButton button = new JButton(t + " " + j);
-               button.addActionListener(new Listener());
-               panel2.add(button);
-            
+            if ((((Square) source).getMinesTouching() == 0) && !((Square) source).isMine()) {
+               System.out.println("getMinesTouching == 0.........");
+               lookDisplaySafeSquares(((Square) source));
+               checkPlayboardResults();
+            } else {
+               ((Square) source).display(displayIcons);
+               ((Square) source).setEnabled(false);
+               if (((Square) source).isMine()) {
+                  System.out.println("HIT the mine.........");
+                  disablePlayboard();
+                  driver.actionPlayboard_Panel(false);
+               } else {
+                  System.out.println("DID NOT HIT the mine.........");
+                  checkPlayboardResults();
+               }
             }
          }
-         Set<String> hash_Set = new HashSet<String>();
-
-         
-         
-      }
-      public void Initial_Generate()
-   {
-      /// Generates the initial map with each square being empty
-         
-         
       
-   }
-      public static void Final_Generate()
-   {
-      /// This method is called when the first square is revealed. This places all the mines. 
-   }
-      public int Mine_Check(int count, int y)
-   {
-      /// Starts by setting a count to 0. Then it assigns each square a number based on how many mines are in its vicinity. It then returns the count.
-      count = 0;
-      return count;
-   }
-      public static void Spread(int x, int y)
-   {
-      /// All of the surrounding squares display themselves. 
-      /// It also checks for victory and tells the game over panel the result.
-      JFrame frame = new JFrame("Mine Sweeper");
-      frame.setSize(800, 200);
-      frame.setLocation(150, 100);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setContentPane(new Game_Over_Panel());
-      frame.setVisible(true); 
-   }
-        private class Listener implements ActionListener
-      {
-         public void actionPerformed(ActionEvent e)
-         {
-            // generates a map of empty squares
+      };
+
+   private int checkPlayboardResults() {
+      int ret = 1;
+   
+      outer: for (int x = 0; x < cellsInSide; x++) {
+         for (int y = 0; y < cellsInSide; y++) {
+            if (squares[x][y].isEnabled()) {
+               if (squares[x][y].isFlagged()) {
+                  if (squares[x][y].isMine()) {
+                  	// System.out.println("Mine flagged properly: " + squares[x][y].toString());
+                     ret = 1;
+                  } else {
+                  	// System.out.println("Mine flagged improperly: " + squares[x][y].toString());
+                     ret = 0;
+                     break outer;
+                  }
+               } else {
+                  System.out.println("Play on.....");
+                  ret = 2;
+                  break outer;
+               }
+            }
          }
       }
+   
+      if (ret == 0) {
+         System.out.println("LOOooOSER");
+         disablePlayboard();
+         driver.actionPlayboard_Panel(false);
+      } else if (ret == 1) {
+         System.out.println("WINNER---------------");
+         disablePlayboard();
+         driver.actionPlayboard_Panel(true);
+      } else {
+         System.out.println("...... do not give up.......");
+      }
+   
+      return ret;
+   }
+
+   private void lookDisplaySafeSquares(Square sq) {
+      if (checked.add(sq.toString())) {
+      
+         sq.setEnabled(false);
+         sq.display(displayIcons);
+      
+         System.out.println("lookDisplaySafeSquares around: " + sq.toString());
+      
+         Square[] neighbors = Square.squareNeighbors(squares, sq);
+         for (int j = 0; j < neighbors.length; j++) {
+            if (neighbors[j] != null) {
+            	/////////////////////////////////
+               if ((neighbors[j].getMinesTouching() != 0) && (!neighbors[j].isMine())
+               		&& (!neighbors[j].isFlagged())) {
+                  neighbors[j].display(displayIcons);  
+                  neighbors[j].setEnabled(false);  
+               }
+            	/////////////////////////////////
+            
+               if ((neighbors[j].getMinesTouching() == 0) && (!neighbors[j].isMine())
+               		&& neighbors[j].isEnabled()) {
+                  lookDisplaySafeSquares(neighbors[j]);
+               }
+            }
+         }
+      }
+   }
+
+   private void disablePlayboard() {
+      for (int x = 0; x < cellsInSide; x++) {
+         for (int y = 0; y < cellsInSide; y++) {
+            squares[x][y].setEnabled(false);
+         }
+      }
+   }
+
+   MouseListener mouseListener = 
+      new MouseAdapter() {
+         public void mouseReleased(MouseEvent mouseEvent) {
+            Object source = mouseEvent.getSource();
+         
+            if (SwingUtilities.isRightMouseButton(mouseEvent)) {
+               if ((start_X != -1) && (((Square) source).isEnabled())) {
+               
+                  if (((Square) source).isFlagged()) {
+                     ((Square) source).displayOrig();
+                     ((Square) source).setFlagged(false);
+                     numFlagsUsed--;
+                     PlaySound.playSoundFile("sounds_flag.wav", 0);
+                  } else {
+                     PlaySound.playSoundFile("sounds_flag.wav", 0);
+                     if (numFlagsUsed < mines) {
+                        ((Square) source).displayFlag(displayIcons);
+                        ((Square) source).setFlagged(true);
+                        numFlagsUsed++;
+                        if (numFlagsUsed == mines) {
+                           checkPlayboardResults();
+                        }
+                     } else {
+                        System.out.println("All flags used: " + numFlagsUsed);
+                        showMessage("All flags have been used.");
+                     }
+                  }
+               }
+            }
+            System.out.println();
+         }
+      };
+
+   private void showMessage(String msg) {
+      JOptionPane.showMessageDialog(this, msg);
+   }
 }
