9fe022508dea2ad689f13579b907b91959ebc939
Hardeep Mann <33522407+HardeepMann@users.noreply.github.com>
2021-05-10 13:10:19 -0400

    Create PlaySound.java

---
+++ PlaySound.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class PlaySound {
+
+   public static void playSoundGameOver(boolean isWinner, int loops) {
+   
+      String fileName = null;
+      if (isWinner) {
+         fileName = "snare.wav";
+      } else {
+         fileName = "mine1.wav";
+      }
+   
+      playSoundFile(fileName, loops);
+   }
+
+   public static void playSoundFile(String file, int loops) {
+      AudioInputStream audioInputStream = null;
+   
+      Clip clip = null;
+   
+      try {
+         java.net.URL url = null;
+      
+         url = PlaySound.class.getResource(file);
+         audioInputStream = AudioSystem.getAudioInputStream(url);
+      	
+         clip = AudioSystem.getClip();
+         clip.open(audioInputStream);
+      
+      } catch (LineUnavailableException e) {
+      	// TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch (IOException e) {
+      	// TODO Auto-generated catch block
+         e.printStackTrace();
+      
+      } catch (UnsupportedAudioFileException e1) {
+      	// TODO Auto-generated catch block
+         e1.printStackTrace();
+      }
+   
+      clip.loop(loops);
+      clip.flush();
+   }
+}
