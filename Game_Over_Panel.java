f07283652eb043d621a53679a655452022745acb
Hardeep Mann <33522407+HardeepMann@users.noreply.github.com>
2021-05-10 13:12:23 -0400

    Update Game_Over_Panel.java

--- Game_Over_Panel.java
+++ Game_Over_Panel.java
@@ -1,64 +1,63 @@
-  //Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
-   //Last Updated: 5/6/2021
-   //Period: 2
-   import javax.swing.*;
-   import java.awt.*;
-   import java.awt.event.*;
-   import javax.swing.JFrame;
-public class Game_Over_Panel extends JPanel
-{
-   //This panel is shown to the user when the game is ended.
-      private JLabel label1;
-      private String result;
-      public Game_Over_Panel()
-      {
-            // sets up the panel
-            setLayout(new FlowLayout());
-            
-            JButton button1 = new JButton("Try Again");
-            button1.addActionListener(new Listener1());
-            add(button1);
-            
-            JButton button2 = new JButton("Exit");
-            button2.addActionListener(new Listener2());
-            add(button2);
-            
-            label1 = new JLabel(" ");
-            label1.setFont(new Font("Times New Roman", Font.BOLD, 100));
-            label1.setForeground(Color.blue);
-            getResult();
-            add(label1);
+
+//Name: Hardeep Mann, Jonluke O`Cain, and Vayun Malik
+//Last Updated: 4/29/2021
+//Period: 2
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Game_Over_Panel extends JPanel {
+   private Minesweeper_Driver driver;
+
+	// This panel is shown to the user when the game is ended.
+   public Game_Over_Panel(Minesweeper_Driver driver, boolean isWinner) throws IOException {
+      this.driver = driver;
+   
+      setLayout(new FlowLayout(FlowLayout.CENTER));
+   
+      JPanel p1 = new JPanel();
+      add(p1);
+      p1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), ""));
+   
+      BufferedImage resultImg;
+   
+      if (isWinner) {
+         resultImg = ImageIO.read(this.getClass().getResource("gold.png"));
+      } else {
+         resultImg = ImageIO.read(this.getClass().getResource("mine_medium.png"));
       }
-   private class Listener1 implements ActionListener{
-      public void actionPerformed(ActionEvent e)
-      {
-         // the user can click this button to try again
-         JFrame frame = new JFrame("Mine Sweeper");
-         frame.setSize(800, 300);
-         frame.setLocation(150, 100);
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.setContentPane(new Start_Up_Panel());
-         frame.setVisible(true);
+   
+      PlaySound.playSoundGameOver(isWinner, 1);
+   
+      JLabel resultLabel = new JLabel(new ImageIcon(resultImg));
+      p1.add(resultLabel);
+   
+      JButton button1 = new JButton("Try Again");
+      button1.addActionListener(new Listener1());
+      add(button1);
+   
+      JButton button2 = new JButton("Exit");
+      button2.addActionListener(new Listener2());
+      add(button2);
+   
+   }
+
+   private class Listener1 implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         System.out.println("Here1");
+         driver.actionfromGameover_Panel(true);
+      
+      	/// the user can click this button to try again
       }
-    }
-    
-    private class Listener2 implements ActionListener{
-      public void actionPerformed(ActionEvent e)
-      {
-         // the user can click this button to exit the program
+   }
+
+   private class Listener2 implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+      	/// the user can click this button to exit the program
          System.exit(0);
       }
-     }
-      public void getResult()
-    {
-      result = "lose";
-      // This method gets the result of whether its a win or a loss from the play board panel. 
-      // Then it will use that result to display the correct text for the user.
-      if (result == "win")
-      {
-         label1.setText("Congratulations You Won!!");
-      }
-      else
-         label1.setText("Sorry, You Lost.");
-    }
    }
+}
